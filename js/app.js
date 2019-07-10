984c2a03fe8925265165e4e16377aa3d89e0bf1e
zubairalamsub <zuabiralam025@gmail.com>
2019-07-10 14:06:45 +0600

    Delete option added

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ console.log("In myController");
 
 $scope.newUser={};
 $scope.clickedUser={};
+$scope.message="";
 $scope.users=[
    { userName: "zubair",fullName:"Md.Zubair Alam",email:"zubairalam0252gmail.com"},
    { userName: "Imane",fullName:"Emmy Amouna",email:"emmy@gmail.com"}
@@ -14,6 +15,7 @@ $scope.saveUser=function()
 {
     $scope.users.push($scope.newUser);
     $scope.newUser={};
+    $scope.message="New User Added Successfully"
 };
 $scope.selectUser=function(user)
 {
@@ -21,8 +23,23 @@ console.log(user);
 $scope.clickedUser=user;
 };
 $scope.updateUser=function(){
-
+$scope.message="Successfully Updated";
 
 };
+
+$scope.clearMessage=function(){
+$scope.message="";
+
+};
+
+
+   $scope.deleteUser = function(user) { 
+    var index = $scope.users.indexOf(user);
+    $scope.users.splice(index, 1);
+    $scope.message="Successfully Deleted"; 
+    
+    
+  };
+
 
 });
